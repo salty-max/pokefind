96f6303e47b3a39bd5c3a30677409740348b9925
max-jellycat <max@jellycat.fr>
2018-01-04 19:37:32 +0100

    added functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 var app = {
 
+  pokeFound: ['marill'],
+  count: 0,
+  thumbs: $('.imgs'),
+
   init: function() {
 
     $(document).keypress(function(key) {
       if(key.which === 13 && $('#user-input').is(':focus')) {
         var input = $('#user-input').val().toLowerCase();
-        $('#user-input').val("");
+        $('#user-input').val('');
         app.checkPokemon(input);
       }
     });
@@ -20,66 +24,61 @@ var app = {
 
   },
 
-  gameOver: function() {
-
-    setTimeout(function() {location.reload()}, 5000);
-    app.playSound('gameover');
-    warning.html('<p>Game Over!</p>');
-
-  },
+  // gameOver: function() {
+  //
+  //   setTimeout(function() {location.reload()}, 5000);
+  //   app.playSound('gameover');
+  //   warning.html('<p>Game Over!</p>');
+  //
+  // },
 
   showLives: function() {
 
     var lives = 3;
     var livesCount = $('.lives');
     switch(lives){
-      case 3:
-        livesCount.empty();
-        livesCount.append('<ul>');
-        for(var i = 0; i < lives; i++) {
-          livesCount.append('<li>&hearts;</li>');
-        }
-        livesCount.append('</ul>');
-        break;
+    case 3:
+      livesCount.empty();
+      livesCount.append('<ul>');
+      for(var i = 0; i < lives; i++) {
+        livesCount.append('<li>&hearts;</li>');
+      }
+      livesCount.append('</ul>');
+      break;
 
-      case 2:
-        livesCount.empty();
-        livesCount.append('<ul>');
-        for(var i = 0; i < lives; i++) {
-          livesCount.append('<li>&hearts;</li>');
-        }
-        livesCount.append('</ul>');
-        break;
+    case 2:
+      livesCount.empty();
+      livesCount.append('<ul>');
+      for(var j = 0; j < lives; j++) {
+        livesCount.append('<li>&hearts;</li>');
+      }
+      livesCount.append('</ul>');
+      break;
 
-      case 1:
-        livesCount.empty();
-        livesCount.append('<ul>');
-        for(var i = 0; i < lives; i++) {
-          livesCount.append('<li>&hearts;</li>');
-        }
-        livesCount.append('</ul>');
-        break;
+    case 1:
+      livesCount.empty();
+      livesCount.append('<ul>');
+      for(var k = 0; k < lives; k++) {
+        livesCount.append('<li>&hearts;</li>');
+      }
+      livesCount.append('</ul>');
+      break;
 
-      default:
-        livesCount.empty();
-        break;
+    default:
+      livesCount.empty();
+      break;
     }
 
   },
 
   checkPokemon: function(input) {
 
-    var images = $('.imgs');
-    var warning = $('.warning');
-    var output = $('.count')
-    var count = 0;
-    var pokeFound = ['marill'];
+    for(var j = 0; j < app.pokeFound.length; j++){
 
-    for(var j = 0; j < pokeFound.length; j++){
+      if(app.pokeFound.includes(input)) {
 
-      if(input === pokeFound[j]) {
         console.log('already found');
-        warning.html('<p>Déjà trouvé!</p>');
+        app.warning('<p>Déjà trouvé!</p>');
         app.playSound('lose');
         //app.lives--;
         // showLives();
@@ -87,28 +86,45 @@ var app = {
         //   gameOver()
         // }
       }
+
       else {
+
         for(var i = 0; i < pokemons.length; i++) {
-          // if(input === pokemons[i] && !pokeFound.includes(pokemons[i])) {
-          //   warning.html('');
-          //   pokeFound.push(input);
-          //   console.log(input);
-          //   input='';
-          //   console.log(pokeFound);
-            images.append('<img class="pokemon" src="img/'+ pokemons[i] +'.png" alt="'+ pokemons[i] +'"/>');
-        //     app.playSound(pokemons[i]);
-        //     count++;
-        //     output.html('<p class="text-help">Pokemon trouvés : <em>'+ count +'</em> sur '+ pokemons.length +'</p>');
-        //   }
+
+          if(input === pokemons[i] && !app.pokeFound.includes(input)) {
+
+            app.warning('');
+            app.addToArray(app.pokeFound, pokemons[i]);
+            console.log(input);
+            input='';
+            console.log(app.pokeFound);
+            app.thumbs.append('<img class="pokemon" src="img/'+ pokemons[i] +'.png" alt="'+ pokemons[i] +'"/>');
+            app.playSound(pokemons[i]);
+            app.count++;
+            app.output(app.count, pokemons.length);
+
+          }
         }
       }
     }
   },
-}
+
+  warning: function(text) {
+    $('.warning').html(text);
+  },
+
+  output: function(count, length) {
+    $('.count').html('<p class="text-help">Pokemon trouvés : <em>'+ count +'</em> sur '+ length +'</p>');
+
+  },
+
+  addToArray: function(arr, element) {
+    arr.push(element);
+  }
+};
 
 $(function() {
 
   $(app.init);
   // $(app.showLives);
-
-})
+});
